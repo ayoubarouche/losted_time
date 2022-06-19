77ecfa5fc64e429b0ffbe2ab155b5c84f1d77b8e
ayoubarouche <aarouche2016@gmail.com>
2022-06-19 23:31:49 +0100

    fixing the reset of the timer after a while

--- content.js
+++ content.js
@@ -1,35 +1,95 @@
-let pages_and_time = {};
+let is_exist = false;
+
+
+// chrome.runtime.onMessage.addListener((msg, sender, response) => {
 
 
 
-chrome.runtime.onMessage.addListener((msg, sender, response) => {
+//     if ((msg.from === 'popup')) {
+
+//         if (!msg.current_link) {
+//             return;
+//         }
+
+//         if (msg.subject == 'how_much_time') {
+//             chrome.storage.sync.get([msg.current_link],function(result){
+//                 response({time:result[msg.current_link]}) ; 
+//             });
+
+//         }
+
+//         if(msg.subject=='set_time'){
+//             let link = msg.current_link ; 
+//             let value_to_set = {} ;
+//             value_to_set[link]= let_time ;  
+//             let let_time = new Date().getTime() ; 
+//             chrome.storage.sync.set({link:let_time}, function(){
+//                 response({time:let_time});
+//             }) ; 
+            
+//         }
+
+//         console.log("from the popup");
+//         console.log("the request link is : ");
+
+//         let new_link = msg.current_link;
+//         // console.log(msg.current_link);
+//         // let answer = undefined; 
+//         // console.log("the current link is : ") ; 
+//         // console.log(msg.current_link); 
+//         // if ((answer=get_pages_and_time(msg.current_link)) != undefined) {
+//         //     console.log("the link already exist :)");
+
+//         //     console.log("the answer is : ");
+//         //     console.log(answer) ;
+//         //     response({ time: answer });
+//         let new_time;
+//         // }
+//         // else {
+//         //     let date = new Date();
+//         //     console.log("register a new link");
+//         //     pages_and_time[msg.current_link] = date;
+//         //     link = msg.current_link;
+//         //     console.log("saving date is : ");
+//         //     console.log(date.getTime()) ;  
+//         //     save_pages_and_time({link : date.getTime()})
+//         //     response({ time: date });
+//         // }
+
+//         chrome.storage.sync.get([new_link], function (result) {
+//             if (result[new_link] == undefined) {
+
+//                 console.log("the value not exist : ");
+//                 console.log(result[new_link]);
+//                 is_exist = false;
+//             }
+//             else {
+//                 is_exist = true;
+//                 new_time = result[new_link];
+//                 console.log("the result is not empty it's contains : ");
+//                 console.log(result[new_link]);
+//                 response({ time: result[new_link] });
+//                 // format_time(result[new_link]) ; 
+//             }
+//         });
+
+//         if (!is_exist) {
+//             console.log("the result is empty");
+//             let time_new = new Date().getTime();
+//             chrome.storage.sync.set({ new_link: time_new }, function () {
+//                 console.log("the sitted time before is :  ");
+//                 console.log(time_new);
+
+//             });
+//             response({ time: time_new });
+//         } else {
+//             response({ time: new_time });
+//         }
 
 
+//     }
 
-    if ((msg.from === 'popup') && (msg.subject === 'how_much_time')) {
-
-        console.log("from the popup");
-        console.log("the request link is : ");
-        if (!msg.current_link) {
-            return;
-        }
-        console.log(msg.current_link);
-        if (pages_and_time[msg.current_link] != undefined) {
-            console.log("the link already exist :)");
-            response({ time: pages_and_time[msg.current_link] });
-
-        }
-        else {
-            let date = new Date();
-            console.log("register a new link");
-            pages_and_time[msg.current_link] = date;
-            response({ time: date });
-        }
-
-
-    }
-
-});
+// });
 
 
 function save_pages_and_time(website_and_time) {
@@ -38,9 +98,9 @@ function save_pages_and_time(website_and_time) {
     });
 }
 
-function get_pages_and_time(website){
-    chrome.storage.sync.get([website], function(result) {
-        console.log('Value currently is ' +result.website);
-        return result.key; 
-      });
+function get_pages_and_time(website) {
+    chrome.storage.sync.get(website, function (result) {
+        console.log('Value currently is ' + result[website]);
+        return result[website];
+    });
 }
--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 let link;
-let link_time;
+let link_time ;
 // asking for the current link from the each tab script : 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -24,16 +24,32 @@ window.addEventListener('DOMContentLoaded', () => {
 // function to get the current time frome the content ( main script )
 
 function format(info) {
-    let link = info.link;
-    chrome.runtime.sendMessage(
-        {
-            from: 'popup',
-            subject: 'how_much_time',
-            current_link: link
+    let new_link = info.link;
 
-        }, function (response) {
-            format_time(response.time);
-        });
+  
+    chrome.storage.sync.get([new_link], function (result) {
+        if (result[new_link]==undefined) {
+            console.log("the result is empty");
+            let time_new = new Date().getTime();
+            let value_to_set = {} ;
+            value_to_set[new_link] = time_new ; 
+
+            chrome.storage.sync.set(value_to_set, function () {
+                console.log("the sitted time before is :  ");
+                console.log(time_new);
+                format_time(time_new); 
+            });
+        }
+        else {
+            console.log("the result is not empty it's contains : ");
+            console.log(result[new_link]);
+            // response({time:result[new_link]});
+            format_time(result[new_link]) ; 
+            // format_time(result[new_link]) ; 
+        }
+    });
+
+
 }
 
 
@@ -42,8 +58,8 @@ function format(info) {
 
 function clock() {
     link_time.setSeconds(link_time.getSeconds() + 1);
-    console.log("inside clock ");
-    console.log(link_time);
+    // console.log("inside clock ");
+    // console.log(link_time);
     let hrs = link_time.getHours();
     let mins = link_time.getMinutes();
     let secs = link_time.getSeconds();
@@ -59,11 +75,11 @@ function clock() {
 
 
 function format_time(time) {
-    console.log("the current time is : ") ; 
-    console.log(time);
+    // console.log("the current time is : ") ; 
+    // console.log(time);
     link_time = new Date(time);
-    console.log("the time received is : ");
-    console.log(link_time.getSeconds());
+    // console.log("the time received is : ");
+    // console.log(link_time.getSeconds());
     let current_time = new Date();
     link_time = new Date(Math.abs(current_time - link_time));
     setInterval(clock, 1000);
@@ -72,7 +88,35 @@ function format_time(time) {
 }
 
 
+function reset_timer(current_link) {
 
+    console.log("you clicked reset ");
+    console.log("the link to remove is : ");
+    console.log(current_link);
+    chrome.storage.sync.remove(current_link, function (result) {
+
+    });
+
+}
+
+document.getElementById("reset").addEventListener("click", function () {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, tabs => {
+        chrome.tabs.sendMessage(
+            tabs[0].id,
+            {
+                from: 'popup', subject: 'link'
+            },
+            function (info) {
+                reset_timer(info.link);
+                // format(info);
+            }
+        );
+    });
+
+});
 // chrome.tabs.sendMessage(
 //     {
 //         from: 'kestar',
