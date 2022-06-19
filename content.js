54734b893648cf36f84a0873ce433e6d76c3cfd1
ayoubarouche <aarouche2016@gmail.com>
2022-06-19 15:37:26 +0100

    working on storage for not losting data

--- content.js
+++ content.js
@@ -10,19 +10,19 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
 
         console.log("from the popup");
         console.log("the request link is : ");
-        if ( ! msg.current_link){
-            return ; 
+        if (!msg.current_link) {
+            return;
         }
         console.log(msg.current_link);
         if (pages_and_time[msg.current_link] != undefined) {
             console.log("the link already exist :)");
             response({ time: pages_and_time[msg.current_link] });
-            
+
         }
         else {
             let date = new Date();
-             console.log("register a new link");
-             pages_and_time[msg.current_link] = date ; 
+            console.log("register a new link");
+            pages_and_time[msg.current_link] = date;
             response({ time: date });
         }
 
@@ -30,3 +30,17 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
     }
 
 });
+
+
+function save_pages_and_time(website_and_time) {
+    chrome.storage.sync.set(website_and_time, function () {
+        console.log('Value is set to ');
+    });
+}
+
+function get_pages_and_time(website){
+    chrome.storage.sync.get([website], function(result) {
+        console.log('Value currently is ' +result.website);
+        return result.key; 
+      });
+}
