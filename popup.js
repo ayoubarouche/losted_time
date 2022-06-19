60da89203b77f4303bf7ad98832004604cbae8c6
ayoubarouche <aarouche2016@gmail.com>
2022-06-19 23:59:49 +0100

    fix the reset problem:

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 let link;
-let link_time ;
+let link_time;
 // asking for the current link from the each tab script : 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -26,25 +26,39 @@ window.addEventListener('DOMContentLoaded', () => {
 function format(info) {
     let new_link = info.link;
 
-  
+
     chrome.storage.sync.get([new_link], function (result) {
-        if (result[new_link]==undefined) {
+        if (result[new_link] == undefined) {
             console.log("the result is empty");
             let time_new = new Date().getTime();
-            let value_to_set = {} ;
-            value_to_set[new_link] = time_new ; 
+            let value_to_set = {};
+            value_to_set[new_link] = {};
+            
+            value_to_set[new_link]['time'] = time_new;
 
+           let current_intervale =  format_time(time_new);
+           value_to_set[new_link]['intervale'] = current_intervale;
+
+           // set the value to set in the storage : 
             chrome.storage.sync.set(value_to_set, function () {
                 console.log("the sitted time before is :  ");
                 console.log(time_new);
-                format_time(time_new); 
+
             });
         }
         else {
             console.log("the result is not empty it's contains : ");
             console.log(result[new_link]);
             // response({time:result[new_link]});
-            format_time(result[new_link]) ; 
+
+            let current_intervale = format_time(result[new_link]['time']);
+            result[new_link]['intervale'] = current_intervale ; 
+            // update the current intervale : 
+            chrome.storage.sync.set(result, function () {
+                console.log("the sitted time before is :  ");
+                console.log(time_new);
+
+            });
             // format_time(result[new_link]) ; 
         }
     });
@@ -82,10 +96,39 @@ function format_time(time) {
     // console.log(link_time.getSeconds());
     let current_time = new Date();
     link_time = new Date(Math.abs(current_time - link_time));
-    setInterval(clock, 1000);
-    document.getElementById("current_time").textContent = link_time;
+    let intervale = setInterval(clock, 1000);
+
+    // save the intervale to clear it when we want that : 
+
+    // chrome.tabs.query({
+    //     active: true,
+    //     currentWindow: true
+    // }, tabs => {
+    //     chrome.tabs.sendMessage(
+    //         tabs[0].id,
+    //         {
+    //             from: 'popup', subject: 'link'
+    //         },
+    //         function (info) {
+
+    //             let value_to_add = {};
+    //             let_intervale = info.link+info.link ; 
+    //             value_to_add[info.link] = intervale ;
+
+    //             chrome.storage.sync.set(value_to_add , function(){
+
+    //             }) ; 
+    //             // format(info);
+    //         }
+    //     );
+    // });
+
+
+    // document.getElementById("current_time").textContent = link_time;
+    return intervale ; 
 
 }
+
 
 
 function reset_timer(current_link) {
@@ -93,8 +136,15 @@ function reset_timer(current_link) {
     console.log("you clicked reset ");
     console.log("the link to remove is : ");
     console.log(current_link);
-    chrome.storage.sync.remove(current_link, function (result) {
+    // first we need to clear the intervale : 
 
+    chrome.storage.sync.get([current_link] , function(result){
+        clearInterval(result[current_link]['intervale']) ; 
+    }) ; 
+
+    // then we want to remove the full link : 
+    chrome.storage.sync.remove(current_link, function () {
+        console.log("the value is removed don't worry !");
     });
 
 }
